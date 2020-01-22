b13bc76b4371aa99551218f72c18096ea7726602
Sergio Vicente <sfvicente@gmail.com>
2020-01-22 23:55:02 +0000

    Added initial version of the LadderOrder class

---
+++ src/BitMEXFormulas.Common/LadderOrder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BitMEXFormulas.Common
+{
+    class LadderOrder
+    {
+        string instrument;
+
+        /// <summary>
+        /// The price you entered at(or the average)
+        /// </summary>
+        decimal startEntryPrice;
+
+        /// <summary>
+        /// The end of the price you entered at(or the average)
+        /// </summary>
+        decimal endEntryPrice;
+
+
+        /// <summary>
+        /// Order quantity in units of the instrument (i.e. contracts).
+        /// </summary>
+        int amount;
+
+        int orderCount;
+
+        int OrderSizeIncreasePercentage;
+
+        /// <summary>
+        /// The amount of leverage used for the order
+        /// </summary>
+        int leverage;
+
+        /// <summary>
+        /// The price where your stoploss is
+        /// </summary>
+        decimal stoplossPrice;
+
+        decimal averagePrice;
+    }
+}
