5113e4b1514c1c00b3736169974089a44f1ee65a
Sergio Vicente <sfvicente@gmail.com>
2020-01-23 17:40:00 +0000

    Added scaffold for the Account class

---
+++ src/BitMEXFormulas.Common/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BitMEXFormulas.Common
+{
+    class Account
+    {
+        decimal walletBalance;
+
+        decimal marginBalance;
+
+        decimal availableBalance;
+    }
+}
