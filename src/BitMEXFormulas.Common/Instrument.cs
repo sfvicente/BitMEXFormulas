5188f0643f67f3231f5312d084e707f44390766d
Sergio Vicente <sfvicente@gmail.com>
2020-01-23 17:49:00 +0000

    Added scaffold for Instrument class

---
+++ src/BitMEXFormulas.Common/Instrument.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BitMEXFormulas.Common
+{
+    public enum Instrument
+    {
+        XBT,
+        ETH,
+        LTC,
+    }
+}
