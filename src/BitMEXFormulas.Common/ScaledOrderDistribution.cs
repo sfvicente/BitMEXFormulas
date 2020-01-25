d2ce2764519faeeb95aa8cc2918c0a5d30b1e036
Sergio Vicente <sfvicente@gmail.com>
2020-01-25 23:50:28 +0000

    Added ScaledOrder distribution enum to control how the order entries are distributed

---
+++ src/BitMEXFormulas.Common/ScaledOrderDistribution.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BitMEXFormulas.Common
+{
+    public enum ScaledOrderDistribution
+    {
+        Flat,
+        Ascending,
+        Descending,
+    }
+}
