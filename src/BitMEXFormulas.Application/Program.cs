207ae583765386c19537bb6b8e4ba88996c706cb
Sergio Vicente <sfvicente@gmail.com>
2020-02-10 23:31:12 +0000

    Added sample application code to test the laddered orders creation

--- src/BitMEXFormulas.Application/Program.cs
+++ src/BitMEXFormulas.Application/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BitMEXFormulas.Common;
+using System.Collections.Generic;
 
 namespace BitMEXFormulas.Application
 {
@@ -6,7 +7,28 @@ namespace BitMEXFormulas.Application
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            decimal accountBalance = 100.5M;
+
+            Order order = new Order()
+            {
+
+            };
+            
+            OrderRiskAssessment orderRiskAssessement = new OrderRiskAssessment(order, accountBalance);
+
+
+            LadderOrder ladderOrder = new LadderOrder();
+
+            ladderOrder.StartEntryPrice = 100;
+            ladderOrder.EndEntryPrice = 200;
+            ladderOrder.OrderCount = 4;
+
+            IEnumerable<Order> orderItems = ladderOrder.CreateFlatOrderItems();
+
+            foreach (var item in orderItems)
+            {
+                System.Console.WriteLine("Item: entry {0}, amount {1}, stoploss{2}, leverage {3}", item.EntryPrice, item.Amount, item.StoplossPrice, item.Leverage);
+            }
         }
     }
 }
